942b5e3a366e76e9fce91194f03dadcf2114017b
Evangelia Tremou <tremou@users.noreply.github.com>
2018-09-27 16:31:21 +0200

    Add files via upload

---
+++ shbeam.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#Statistics for fits files. 
+
+import argparse
+import math
+import numpy as np
+import glob
+import sys
+import casacore
+from casacore.images import *
+from radio_beam import Beam
+from astropy.io import fits   
+
+parser = argparse.ArgumentParser(description='Beam size', epilog="Output:", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("infile", type=argparse.FileType('rb'), help='input FITS image')
+
+args = parser.parse_args()
+parser.print_help()
+
+
+header = fits.getheader(args.infile)
+my_beam = Beam.from_fits_header(header)
+print my_beam
---
+++ stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+#Statistics for fits files. 
+
+import argparse
+import math
+import pyfits
+import numpy as np
+from astropy import units as u
+import pylab
+import glob
+import sys
+import casacore
+from casacore.images import *
+    
+parser = argparse.ArgumentParser(description='Fits image stats', epilog="Output:", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("infile", type=argparse.FileType('rb'), help='input FITS image')
+
+args = parser.parse_args()
+parser.print_help()
+    
+hdulist = pyfits.open(args.infile)
+hdu=hdulist[0]
+data = hdulist[0].data
+header = hdulist[0].header
+hdulist.info()
+hdulist.close()
+max = np.max(data)
+min = np.min(data)
+rms= np.std(data)
+
+#im=casacore.images.image(args.infile)
+#casacore_stat=im.statistics()
+
+print "RMS =", rms, "Jy/beam"
+print "MAX =", max, "Jy/beam"    
+print "MIN =", min, "Jy/beam"        
+#print "casacore stats:", casacore_stat 
+        
+
+#if args != 1:
+	#print 'Please specify a FITS file'
+	#sys.exit(-1)
+	
